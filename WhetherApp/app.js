bd1d7ff6fce39338daa67802b0898e06070296ea
nashafee <naghman.shafique@gmail.com>
2022-12-01 17:49:42 +0500

    Save Wheather App Changes

--- WhetherApp/app.js
+++ WhetherApp/app.js
@@ -1,6 +1,12 @@
 function getWheather() {
     apiKey = "9bde8dd003f38ffd54c7a38be63de3ba";
     let cityName = document.getElementById("text-ip").value;
+    let city     = document.getElementById("city");
+    let country     = document.getElementById("country");
+    let temp     = document.getElementById("temp");
+    let flike     = document.getElementById("flike");
+    let tmin     = document.getElementById("tmin");
+
     let url = `https://api.openweathermap.org/data/2.5/weather?&q=${cityName}&units=metric&appid=9bde8dd003f38ffd54c7a38be63de3ba`
     console.log(url);
 
@@ -11,6 +17,11 @@ function getWheather() {
             console.log(response.data);
             console.log(response.data.name);
             console.log(response.data.main.temp);
+            city.innerHTML += `City:`+ response.data.name;
+            country.innerHTML += `Country:`+ response.data.sys.country;
+            temp.innerHTML += `Temprature:`+ response.data.main.temp;
+            flike.innerHTML += `Feel Like    :`+ response.data.main.feels_like;
+            tmin.innerHTML += `Minumun Tempreture    :`+ response.data.main.temp_min;
         })
         .catch(function (error) {
             // handle error
